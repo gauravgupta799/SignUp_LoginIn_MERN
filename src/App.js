d2d85afab8cb8ba8bae8c77de3f8c3fec4354d1e
gauravgupta <grkml9630@gmail.com>
2022-08-09 10:48:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Signup from "./components/signup/index";
+import LogIn from "./components/Login/index";
+
+
+function App() {
+  return (
+    <div>
+      {/* <Signup/> */}
+      <LogIn/>
+    </div>
+  );
+}
+
+export default App;
